de9e6ab6f3583b6c0b3ba63b2e973fba4ee20fbc
andrey <andrey@terraform.home>
2019-03-22 13:26:28 -0400

    Created a project

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "loc" {
+    description = "Default Azure region"
+    default     =   "eastus2"
+}
+
+variable "tags" {
+    default     = {
+        source  = "citadel"
+        env     = "training"
+    }
+}
